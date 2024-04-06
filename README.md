2a6b16597247c899572e1119285ab8d8476cc298
caosontr <caosontr@gmail.com>
2024-04-06 19:14:03 +0700

    ok

---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+## Getting Started
+
+After clone the repository, we need to install the dependencies:
+
+```bash
+npm install
+```
+
+Then, run the development server:
+
+```bash
+npm run dev
+```
+
+Run json server:
+
+```npm run serve ```
+
+Open [http://localhost:5173/admin/products](http://localhost:5173/admin/products) with your browser to see the result.
---
+++ src/services/product/index.ts
@@ -0,0 +1,34 @@
+import axiosInstance from '@/config/axiosInstance.ts'
+import { Product, ProductFormValue } from '@/types/Product'
+import sleep from '@/ultis/sleep.ts'
+
+const getProducts = async (): Promise<Product[]> => {
+  await sleep()
+  const response = await axiosInstance.get('/products')
+  return response.data
+}
+
+const getProduct = async (id: string): Promise<Product> => {
+  await sleep()
+  const response = await axiosInstance.get('/products/' + id)
+  return response.data
+}
+
+const updateProduct = async (id: string, newValue: ProductFormValue): Promise<Product> => {
+  await sleep()
+  const response = await axiosInstance.put(`/products/${id}`, newValue)
+  return response.data
+}
+
+const createProduct = async (formValue: ProductFormValue): Promise<Product> => {
+  await sleep()
+  const response = await axiosInstance.post('/products', formValue)
+  return response.data
+}
+
+const deleteProduct = async (id: string): Promise<void> => {
+  await sleep()
+  await axiosInstance.delete(`/products/${id}`)
+}
+
+export { getProducts, getProduct, updateProduct, createProduct, deleteProduct }
